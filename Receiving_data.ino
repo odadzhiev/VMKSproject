5345b489f7da587761f76505064e30c0123cfb34
MayaKKoleva <161861698+MayaKKoleva@users.noreply.github.com>
2026-04-14 21:35:51 +0300

    updating receiver code

--- Receiving_data.ino
+++ Receiving_data.ino
@@ -5,20 +5,38 @@ typedef struct struct_message {
     float temperature;
     float pressure;
     float altitude;
+    float totalAccel;
+    float gyroX;
+    float speed;
+    int satellites;
 } struct_message;
 
 struct_message myData;
 
 void OnDataRecv(const esp_now_recv_info *info, const uint8_t *incomingData, int len) {
     memcpy(&myData, incomingData, sizeof(myData));
-    Serial.print("Bytes received: ");
+    Serial.print("Bytes: ");
     Serial.println(len);
-    Serial.print("Temperature: ");
+    Serial.print("Temp: ");
     Serial.println(myData.temperature);
-    Serial.print("Pressure: ");
+    Serial.print("Press: ");
     Serial.println(myData.pressure);
-    Serial.print("Altitude: ");
+    Serial.print("Alt: ");
     Serial.println(myData.altitude);
+    // Serial.print("Acc: ");
+    // Serial.println(myData.totalAccel);
+    // Serial.print("Gyro: ");
+    // Serial.println(myData.gyroX);
+    // Serial.print("Latitude: ");
+    // Serial.println(myData.lat);
+    // Serial.print("Longtitute: ");
+    // Serial.println(myData.lng);
+    // Serial.print("Speed: ");
+    // Serial.println(myData.speed);
+    // Serial.print("Sat: ");
+    // Serial.println(myData.satellites);
+    // Serial.print("Launched: ");
+    // Serial.println(myData.launched);
     
     Serial.println();
 }
